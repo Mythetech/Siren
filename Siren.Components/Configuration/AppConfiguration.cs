19e6cf5b8feeebb27f25a16f7626859122d07a0d
Tom Brewer <tombrewer@Toms-MacBook-Pro.local>
2025-12-27 11:52:24 -0700

    feat: add more helpful links to About dialog Try fix for windows installed icon

---
+++ Siren.Components/Configuration/AppConfiguration.cs
@@ -0,0 +1,12 @@
+namespace Siren.Components.Configuration
+{
+    public class AppConfiguration
+    {
+        public string ReportIssueUrl { get; set; } = "https://github.com/Mythetech/Siren/issues/new?template=bug_report.md";
+        
+        public string CompanyWebsite { get; set; } = "https://www.mythetech.com";
+        
+        public string GitHubUrl { get; set; } = "https://github.com/Mythetech/Siren";
+    }
+}
+
