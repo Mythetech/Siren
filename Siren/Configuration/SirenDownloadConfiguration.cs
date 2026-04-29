33e7c1da2cc3f0e1b25ed1c87858362ac5e5d04d
Tom Brewer <tombrewer0@gmail.com>
2026-04-28 20:17:23 -0600

    ci: switch to shared workflow

--- Siren/Configuration/SirenDownloadConfiguration.cs
+++ Siren/Configuration/SirenDownloadConfiguration.cs
@@ -2,5 +2,5 @@ namespace Siren.Configuration;
 
 public static class SirenDownloadConfiguration
 {
-    public const string UpdateBaseUrl = "https://stsirendownloads.blob.core.windows.net/releases";
+    public const string UpdateBaseUrl = "https://cdn-endpnt-stmythetechglobal.azureedge.net/releases/siren";
 }
